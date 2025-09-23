329ada2949a278ddc89ae19ca8a327b1a6a93a94
JoaoRbs <161484498+JoaoRbs@users.noreply.github.com>
2025-09-23 08:50:28 -0300

    add login

--- app.py
+++ app.py
@@ -4,7 +4,20 @@ app = flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello, World!'
+    return render_template('index.html')
     
+@app.route('/login',methods = ['GET','POST'])
+def login();
+    error=None
+    if request.method =='POST':
+        Username = request.form['username']
+        password = request.form['password']
+        if username == 'admin' and password == 'password'
+            return 'login com sucesso'
+        else:
+            error = 'credenciais inválidas, tente novamente'
+    return render_template('login,html', error=error)
+
 if __name__ == '__main__' :
-    app.run(debug=True)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port, debug=True)
