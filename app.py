300ad4ec6ef33bf789e7ac1b1e6f6f490600a042
JoaoRbs <161484498+JoaoRbs@users.noreply.github.com>
2025-10-02 07:52:01 -0300

    login html 02/10 7:51

--- app.py
+++ app.py
@@ -1,23 +1,24 @@
-from flask import flask
-
-app = flask(__name__)
-
+from flask import Flask, render_template  # Adicione render_template
+app = Flask(__name__)
 @app.route('/')
 def hello_world():
-    return render_template('index.html')
-    
-@app.route('/login',methods = ['GET','POST'])
-def login();
-    error=None
-    if request.method =='POST':
-        Username = request.form['username']
+       return render_template('index.html')
+       
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    error = None
+    if request.method == 'POST':
+        username = request.form['username']  # Removida a vírgula desnecessária
         password = request.form['password']
-        if username == 'admin' and password == 'password'
-            return 'login com sucesso'
+        if username == 'admin' and password == 'password':
+            return 'Login com sucesso'  # Redireciona para home em sucesso
         else:
-            error = 'credenciais inválidas, tente novamente'
-    return render_template('login,html', error=error)
+            error = 'Credenciais inválidas. Tente novamente.'
+    return render_template('login.html', error=error)
 
-if __name__ == '__main__' :
+
+if __name__ == '__main__':
     port = int(os.environ.get('PORT', 5000))
     app.run(host='0.0.0.0', port=port, debug=True)
+
+
